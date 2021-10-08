b37c012e3baf002881a8ae54f8dc6c99859fc442
Mazdak Momen <mmomen@users.noreply.github.com>
2021-10-08 12:47:53 -0700

    Clean up

--- main.py
+++ main.py
@@ -13,10 +13,13 @@ def healthcheck():
         # get healthcheck
         r = requests.get(HEALTHCHECK)
         r.raise_for_status()
-        # print('print status code')
-        print(f'Status code: {r.status_code}')
-        # parse content
+
+        # print(f'Status code: {r.status_code}')
+
+        # get content
         c = r.json()
+
+        # parse content
         for key, value in c.items():
             print(key, ":", value)
             if value == 'ok':
@@ -24,10 +27,9 @@ def healthcheck():
                 break
             else:
                 print(f'Healthcheck was not ok: {value}')
+
     except Exception as e:
         print(f'Healthcheck failed: {e}')
-        # raise
-
 
 
 if __name__ == '__main__':
