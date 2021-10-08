e712530d0b236c29147e13488e8133570a64f491
Mazdak Momen <mmomen@users.noreply.github.com>
2021-10-08 13:14:16 -0700

    add todo method

--- main.py
+++ main.py
@@ -4,33 +4,49 @@ import requests
 from requests.exceptions import HTTPError
 import json
 
-HEALTHCHECK = "https://u3mpbqz6ki.execute-api.us-east-1.amazonaws.com/prod/healthcheck"
-TODO = "https://u3mpbqz6ki.execute-api.us-east-1.amazonaws.com/prod/todo"
-
 
 def healthcheck():
+    healthcheck_url = "https://u3mpbqz6ki.execute-api.us-east-1.amazonaws.com/prod/healthcheck"
     try:
         # get healthcheck
-        r = requests.get(HEALTHCHECK)
+        r = requests.get(healthcheck_url)
         r.raise_for_status()
-
-        # print(f'Status code: {r.status_code}')
-
+        # print(f'HC Status code: {r.status_code}')
         # get content
         c = r.json()
-
         # parse content
         for key, value in c.items():
             print(key, ":", value)
             if value == 'ok':
-                # todo()
-                break
+                todo()
+                # break
             else:
                 print(f'Healthcheck was not ok: {value}')
 
     except Exception as e:
         print(f'Healthcheck failed: {e}')
 
+def todo():
+    todo_url = "https://u3mpbqz6ki.execute-api.us-east-1.amazonaws.com/prod/todo"
+    test_json = json.dumps({'title': 'Game of Thrones season 8 was trash.'})
+
+    try:
+        r = requests.get(todo_url)
+        r.raise_for_status()
+        # print(f'TODO Status code: {r.status_code}')
+        c = r.json()
+        print(f'{c}')
+
+    except Exception as e:
+        print(f'Todo GET request failed: {e}')
+
+    try:
+        p = requests.post(todo_url, test_json)
+        p.raise_for_status()
+        print(p.text)
+
+    except Exception as e:
+        print(f'Todo POST request failed: {e}')
 
 if __name__ == '__main__':
     healthcheck()
